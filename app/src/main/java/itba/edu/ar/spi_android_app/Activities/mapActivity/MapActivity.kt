e9a3dce6d412ff09882cbde5b462979d9f0c85dc
Juan Li Puma <juanlipuma94@gmail.com>
2018-12-11 17:53:05 -0500

    Configure map, ground overlay
    - Set initial camera in ITBA
    - Configure indoor maps (but we don't use them after all)
    - Add ground overlay with test resource

--- app/src/main/java/itba/edu/ar/spi_android_app/Activities/mapActivity/MapActivity.kt
+++ app/src/main/java/itba/edu/ar/spi_android_app/Activities/mapActivity/MapActivity.kt
@@ -7,14 +7,28 @@ import android.location.Location
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
+import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
 import com.orhanobut.logger.Logger
 import itba.edu.ar.spi_android_app.Activities.utils.SingleFragmentActivity
+import itba.edu.ar.spi_android_app.R
 
-class MapActivity : SingleFragmentActivity(), GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener, OnMapReadyCallback {
+class MapActivity : SingleFragmentActivity(), GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener, OnMapReadyCallback, GoogleMap.OnIndoorStateChangeListener {
+    val TAG = "SPI_ITBA"
+
+    override fun onIndoorBuildingFocused() {
+        Log.w(TAG, "Indoor building focused!!")
+    }
+
+    override fun onIndoorLevelActivated(p0: IndoorBuilding?) {
+        Log.w(TAG, "Indoor level activated on building $p0")
+    }
+
     private val RequestFineLocationPermission = 42
 
     override fun onMyLocationClick(location: Location) {
@@ -30,11 +44,16 @@ class MapActivity : SingleFragmentActivity(), GoogleMap.OnMyLocationButtonClickL
         return false
     }
 
+    private val ITBA = LatLng(-34.603500, -58.367791)
     private var map: GoogleMap? = null
-    private var fragment: SupportMapFragment = SupportMapFragment.newInstance()
+    private var fragment: SupportMapFragment = SupportMapFragment.newInstance(GoogleMapOptions()
+        .camera(CameraPosition(ITBA, 18f, 0f, 0f)) // ITBA
+//        .camera(CameraPosition(LatLng(40.643365, -73.781709), 18f, 0f, 0f)) // JFK
+//        .camera(CameraPosition(LatLng(21.332, -157.92), 18f, 0f, 0f)) // Honolulu
+        .mapType(GoogleMap.MAP_TYPE_NORMAL))
 
     override fun createFragment(): Fragment {
-        fragment.getMapAsync (this)
+        fragment.getMapAsync(this)
         return fragment
     }
 
@@ -47,9 +66,37 @@ class MapActivity : SingleFragmentActivity(), GoogleMap.OnMyLocationButtonClickL
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
             map.isMyLocationEnabled = true
+            map.uiSettings.isMyLocationButtonEnabled = true
+//            map.isBuildingsEnabled = true
+//            map.isBuildingsEnabled = false
+            map.isIndoorEnabled = true
+            map.isTrafficEnabled = false
             map.setOnMyLocationButtonClickListener(this)
             map.setOnMyLocationClickListener(this)
-            map.uiSettings.isMyLocationButtonEnabled = true
+            map.setOnIndoorStateChangeListener(this)
+            map.uiSettings.isIndoorLevelPickerEnabled = true
+
+            // Work with indoor maps but this only works with supported buildings, not custom ones
+//            Handler().postDelayed({
+//                Log.i(TAG, "Getting current building...")
+//                Log.i(TAG, "Hello this is second log.")
+//                map.focusedBuilding?.levels?.forEach { Log.i(TAG, "${it.name} - ${it.shortName}") }
+//                Log.i(TAG, "Hello this is third log. Selected building is ${if (map.focusedBuilding == null) "Null" else "Not null"}")
+//                val itba = map.focusedBuilding
+//                Log.i(TAG, "Selected building: $itba")
+//                if (itba != null) {
+//                    val levels = itba.levels
+//                    Log.i(TAG, "Levels: ${if (levels == null) "Null" else "Not null"}")
+//                } else {
+//                    Log.w(TAG, "ITBA is null =(")
+//                }
+//            }, 20 * 1000)
+
+            map.addGroundOverlay(GroundOverlayOptions()
+                    .position(ITBA, 100f)
+                    .image(BitmapDescriptorFactory.fromResource(R.drawable.test))
+//                    .anchor(0f, 0f)
+            )
         } else {
             // Show rationale and request permission.
             Logger.w("Location permission not granted, requesting")
