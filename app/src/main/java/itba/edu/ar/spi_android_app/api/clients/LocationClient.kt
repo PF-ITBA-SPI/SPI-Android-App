39f51f2bb3b451cec07ac68031d22d997894149e
Juan Li Puma <juanlipuma94@gmail.com>
2019-09-12 14:54:45 -0300

    Send scan results to server to query location (#6)
    * Send scan results to server to query for location
    
    * Add TODO

--- app/src/main/java/itba/edu/ar/spi_android_app/api/clients/LocationClient.kt
+++ app/src/main/java/itba/edu/ar/spi_android_app/api/clients/LocationClient.kt
@@ -1,14 +1,11 @@
 package itba.edu.ar.spi_android_app.api.clients
 
-import ar.edu.itba.spi_android_app.api.models.Building
-import ar.edu.itba.spi_android_app.api.models.Sample
+import ar.edu.itba.spi_android_app.api.models.LocationResult
 import io.reactivex.Observable
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Path
 
 interface LocationClient {
     @POST("/location")
-    fun create(@Path("buildingId") buildingId: String, @Body fingerprint: HashMap<String, Double>): Observable<Sample>
+    fun queryLocation(@Body fingerprint: Map<String, Double>): Observable<LocationResult>
 }
